d99b1370e05ee19516d2fe65a384fe40aeec9a37
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-11-11 15:14:13 +0530

    c

--- src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoOct12Application.java
+++ src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoOct12Application.java
@@ -1,13 +1,16 @@
 package com.cg.spring.boot.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ComCgSpringBootDemoOct12Application {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(ComCgSpringBootDemoOct12Application.class, args);
+		
 	    System.out.println("Start");
+	    SpringApplication.run(ComCgSpringBootDemoOct12Application.class, args);
+	    System.out.println("End");
 	}
 
 }
---
+++ src/main/java/com/cg/spring/boot/demo/Hello.java
@@ -0,0 +1,33 @@
+package com.cg.spring.boot.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+	
+	//URL  -  http://localhost:8082/hello
+	//Endpoint, API  - hello
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("hello world!");
+		return "Hello Sridhar!";
+	}
+	@RequestMapping("/hi")
+	public String hi() {
+		System.out.println("hi world!");
+		return "Hi Sridhar!";
+	}
+
+	// http://localhost:8082/some-other-api
+	@RequestMapping("/some-other-api")
+	public String someOtherApi() {
+		System.out.println("someOtherApi");
+		return "<!DOCTYPE html>\r\n" + "<html>\r\n" + "\r\n" + "<head>\r\n" + "</head>\r\n" + "\r\n" + "<body>\r\n"
+				+ "    <h1 style='color: blue; font-family: Arial, Helvetica;'>Welcome to Capgemini</h1>\r\n"
+				+ "    <p style='color: darkblue; font-family: consolas, Monospace;'>You are most welcome to Capgemini.</p>\r\n"
+				+ "    <img src='https://www.capgemini.com/us-en/wp-content/uploads/sites/4/2018/08/cropped-cropped-capgemini_logo_color_rgb.png'>\r\n"
+				+ "</body>\r\n" + "\r\n" + "</html>";
+	}
+}
