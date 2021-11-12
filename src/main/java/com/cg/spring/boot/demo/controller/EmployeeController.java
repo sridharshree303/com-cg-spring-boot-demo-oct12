3b1114d2333b9ecbfeb8b4ae75dc07aca89ba577
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-11-12 12:52:46 +0530

    c

--- src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
@@ -1,32 +1,64 @@
 package com.cg.spring.boot.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.spring.boot.demo.model.Employee;
+import com.cg.spring.boot.demo.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
 
-	
-	
-	@GetMapping("/getemp")
-	public Employee getEmployee(){
-		System.out.println("getEmployee");
-		return new Employee(101,"sridhar",10.7);	
-	}
-	
-	@GetMapping("/getallemp")
-	public List<Employee> getAllEmployees() {
-		System.out.println("getAllEmployees");
-		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 10.5));
-		empList.add(new Employee(102, "Monu", 15.5));
-		empList.add(new Employee(103, "Tonu", 12.5));
-		return empList;
-	}
-}
+	@Autowired
+	private EmployeeService empService;
 
+	// http://localhost:8082/getallemps
+	@GetMapping("/getallemps")
+	public List<Employee> getAllEmps() {
+		System.out.println("Controller getAllEmps");
+		return empService.getAllEmployees();
+	}
+
+	// http://localhost:8082/getempbyid/{eid}
+	@GetMapping("/getempbyid/{eid}")
+	public Employee getEmpById(@PathVariable(name = "eid") int eid) {
+		System.out.println("Controller getEmpById");
+		return empService.getEmployeeById(eid);
+	}
+
+	// http://localhost:8082/addemp
+	@PostMapping("/addemp")
+	public Employee addEmp(@RequestBody Employee employee) {
+		System.out.println("Controller addEmp");
+		return empService.addEmployee(employee);
+	}
+
+//	
+//	@GetMapping("/getemp")
+//	public Employee getEmployee(){
+//		System.out.println("getEmployee");
+//		return new Employee(101,"sridhar",10.7);	
+//	}
+//	
+//	@PostMapping("/getallemp")
+//	public List<Employee> getAllEmployees() {
+//		System.out.println("getAllEmployees");
+//		List<Employee> empList = new ArrayList<>();
+//		empList.add(new Employee(101, "Sonu", 10.5));
+//		empList.add(new Employee(102, "Monu", 15.5));
+//		empList.add(new Employee(103, "Tonu", 12.5));
+//		return empList;
+//	}
+//	@RequestMapping("/rrrrr")
+////@GetMapping("/ggggg")
+////@PostMapping("/aaaa")
+////@PutMapping("/ppp")
+////@DeleteMapping("/ddddd")
+//
+}
--- src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
+++ src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
@@ -1,9 +1,55 @@
 package com.cg.spring.boot.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cg.spring.boot.demo.model.Employee;
+import com.cg.spring.boot.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
 
-	
+	@Autowired
+	private EmployeeRepository empRepository;
+
+	public List<Employee> getAllEmployees() {
+		System.out.println("Service getAllEmployees");
+		return empRepository.findAll();
+	}
+
+	public Employee getEmployeeById(int eid) {
+		System.out.println("Service getEmployeeById");
+		return empRepository.findById(eid).get();
+	}
+
+	public Employee addEmployee(Employee employee) {
+		return empRepository.save(employee);
+	}
+
+//	private List<Employee> empList = new ArrayList<>();
+//
+//	{
+//		empList.add(new Employee(101, "Sonu", 10.5));
+//		empList.add(new Employee(102, "Monu", 15.5));
+//		empList.add(new Employee(103, "Tonu", 12.5));
+//	}
+//
+//	public List<Employee> getAllEmployees() {
+//		System.out.println("Service getAllEmployees");
+//		return empList;
+//	}
+//	
+//	public Employee getEmployeeById(int eid) {
+//		System.out.println("Service getEmployeeById");
+//		return empList.stream().filter(e -> eid == e.getEid()).findAny().orElse(null);
+//	}                     // if u add or.else()  then it will show null instead of error
+//                          // give out of range 104
+//	
+//	public Employee addEmployee(Employee employee) {
+//		empList.add(employee);
+//		return employee;
+//	}
 }
