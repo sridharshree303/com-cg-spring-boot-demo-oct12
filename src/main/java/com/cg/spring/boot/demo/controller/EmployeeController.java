22e25aa83e3abd6734dbe32338a4e504196b393e
kolluru sridhar <87171968+sridharshree303@users.noreply.github.com>
2021-12-09 22:49:12 +0530

    commit
    added common requesting mapUrl

--- src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.spring.boot.demo.model.AppUser;
@@ -21,6 +22,7 @@ import com.cg.spring.boot.demo.model.Employee;
 import com.cg.spring.boot.demo.service.EmployeeService;
 
 //@Component
+@RequestMapping("/emp")
 @RestController
 public class EmployeeController {
 
